fb557d3100970e662b332115707a8ee66fdfff17
Diogo Marques <diogo.amv19@gmail.com>
2022-05-08 17:30:43 -0300

    :wrench: backend port

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-// the application listens to port 3333
-app.listen(3333, () => {
+// the application listens to env PORT or 3333
+app.listen(process.env.PORT || 3333, () => {
   console.log("HTTP server running");
 });
